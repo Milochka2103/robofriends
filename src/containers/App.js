a00f6aae34d333607e853cd0d6100f5993e62b1f
Milochka2103 <96441851+Milochka2103@users.noreply.github.com>
2023-04-19 20:12:53 +0300

    fixed error

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import { Scroll } from "../components/Scroll";
 import ErrorBoundry from "../components/ErrorBoundry";
-import { robots } from "../robots";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import tachyons from 'tachyons';
 import App from './containers/App';
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
